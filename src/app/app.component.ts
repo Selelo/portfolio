de604217653716f7e3c9e999cf406b3b874b33ba
Selelo <mogapisel@gmail.com>
2024-05-29 13:04:24 +0200

    add content pt. 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -15,24 +15,5 @@ import { isPlatformBrowser } from '@angular/common';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'portfolio';
-  constructor(@Inject(PLATFORM_ID) private platformId: Object) {}
-
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    if (isPlatformBrowser(this.platformId)) {
-      const scrollPosition = window.scrollY || document.documentElement.scrollTop || document.body.scrollTop || 0;
-      const heroSection = document.getElementById('hero')?.offsetTop;
-      const projectsSection = document.getElementById('projects')?.offsetTop;
-      const aboutSection = document.getElementById('about')?.offsetTop;
-
-      if (scrollPosition < heroSection!) {
-        window.scrollTo({ top: 0, behavior: 'smooth' });
-      } else if (scrollPosition < projectsSection!) {
-        window.scrollTo({ top: heroSection, behavior: 'smooth' });
-      } else if (scrollPosition < aboutSection!) {
-        window.scrollTo({ top: projectsSection, behavior: 'smooth' });
-      }
-    }
-  }
+  title = `Selelo Mogapi's Portfolio`;
 }
--- src/app/components/projects/projects.component.ts
+++ src/app/components/projects/projects.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { SafeHtmlPipe } from '../../pipes/safe-html.pipe';
 
 @Component({
   selector: 'app-projects',
   standalone: true,
-  imports: [],
+  imports: [SafeHtmlPipe],
   templateUrl: './projects.component.html',
   styleUrl: './projects.component.scss'
 })
@@ -13,16 +14,58 @@ export class ProjectsComponent {
 
 export const WORKS = [
   {
-    title: 'Project 1',
-    description: 'Description of Project 1',
-    imageUrl: '/assets/screenshot1.png',
-    link: 'http://example.com/project1'
+    title: 'Absa Insurance Self-Service Portal',
+    description: `<div>
+    <p>The Absa Insurance Self-Service Portal is a user-friendly web application designed for Absa customers to manage their insurance policies online. This convenient platform allows users to:</p>
+    <ul>
+      <li><b>View and Update Policies</b>: Customers can easily access detailed information about their insurance policies, including coverage details, premium amounts, and renewal dates. They can also make updates to their personal information and policy preferences.</li>
+      <li><b>File and Track Claims</b>: The portal streamlines the claims process, enabling users to submit claims online and track their status in real-time, ensuring a hassle-free experience during stressful times.</li>
+      <li><b>Make Payments</b>: Users can securely pay their insurance premiums through the portal, with options for one-time payments or setting up automatic payments for added convenience.</li>
+      <li><b>Download Documents</b>: Important documents such as policy statements, claims reports, and payment receipts can be downloaded directly from the portal, keeping everything organized and easily accessible.</li>
+    </ul>
+    <p>This web app is intended for Absa insurance policyholders who seek a straightforward, digital solution to manage their insurance needs anytime, anywhere. It's ideal for individuals who prefer the convenience of handling their insurance matters online rather than through traditional phone calls or in-person visits.</p>
+  </div>
+  `,
+    imageUrl: '/assets/aic_ss.jpg',
+    link: 'https://insurance.absa.africa/iss/'
   },
   {
-    title: 'Project 2',
-    description: 'Description of Project 2',
-    imageUrl: '/assets/screenshot2.png',
-    link: 'http://example.com/project2'
+    title: 'Absa Insurance Instant Quote',
+    description: `<div>
+    <p>The Absa Insurance Instant Quote web application is a fast and efficient tool designed to provide potential customers with insurance quotes in under 60 seconds. This user-friendly platform offers:</p>
+    <ul>
+      <li><b>Rapid Quotes</b>: By entering minimal information about themselves and their insurance requirements, users receive an instant, personalized insurance quote within seconds, making it quick and convenient.</li>
+      <li><b>Comparison of Options</b>: The app presents various coverage options and premium rates, allowing users to compare and select the plan that best suits their needs and budget.</li>
+      <li><b>Streamlined Application Process</b>: Satisfied with their quote, users can immediately apply for the insurance policy through the app, ensuring a seamless transition from quote to coverage.</li>
+      <li><b>Detailed Product Information</b>: Users can access comprehensive details about different insurance products, including benefits and exclusions, aiding in informed decision-making.</li>
+    </ul>
+    <p>This web app is intended for potential Absa insurance customers who want a quick and easy way to get insurance quotes. It is ideal for individuals seeking new insurance policies and preferring the convenience and speed of a digital-first solution.</p>
+  </div>
+  `,
+    imageUrl: '/assets/coming_soon.jpeg',
+    link: 'https://insurance.absa.africa/iss/instantquote/'
   },
+  {
+    title: 'InfoByte Technologies',
+    description: `<div>
+    <p>Infobyte Tech's website, developed using a PHP theme, represents a comprehensive and customized online presence for the company. The development process included:</p>
+    <ul>
+      <li><b>Customization</b>: The PHP theme was extensively customized to align with Infobyte Tech's branding and functional requirements, creating a unique and engaging user experience.</li>
+      <li><b>Database Integration</b>: A MySQL database was integrated and uploaded to manage the website's data efficiently, ensuring smooth performance and reliable data handling.</li>
+      <li><b>Deployment</b>: The website was deployed to a cPanel server, leveraging cPanel's robust hosting environment for ease of management and enhanced security.</li>
+      <li><b>DNS Management</b>: The process included updating all DNS records and handling domain migration to ensure a seamless transition and minimal downtime, maintaining consistent website accessibility.</li>
+    </ul>
+    <p>This meticulous development effort showcases my technical capabilities and commitment to delivering high-quality digital solutions. The website serves as a central hub for the company's services, providing valuable information and resources their clients and visitors.</p>
+  </div>
+  `,
+    imageUrl: '/assets/infobyte.jpg',
+    link: 'https://infobytetech.co.za/'
+  },
+  {
+  title: 'InfoByte Technologies',
+  description: ``,
+  imageUrl: '../../../assets/coming_soon.jpeg',
+  link: 'https://infobytetech.co.za/'
+},
   // Add more projects as needed
 ];
