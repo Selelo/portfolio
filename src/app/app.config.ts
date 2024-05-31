31239b3ab9e38bec54636f4a34119fc363a4aa73
Selelo <mogapisel@gmail.com>
2024-05-31 12:38:46 +0200

    Add Material UI

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,8 @@ import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), importProvidersFrom([BrowserAnimationsModule])] // BrowserAnimations and legacy modules should be imported this way.
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(), importProvidersFrom([BrowserAnimationsModule]), provideAnimationsAsync()] // BrowserAnimations and legacy modules should be imported this way.
 };
